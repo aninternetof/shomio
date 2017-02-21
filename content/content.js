0f740987b1bb57064a2aa2aa64c74f881412638a
aninternetof <bradyhurlburt@gmail.com>
2017-02-21 15:53:49 -0500

    Adds Vue templating and edit modal

--- content/content.js
+++ content/content.js
@@ -1,1 +1,1 @@
-var contentJson = [{"title":"NXP Thing","content":"Lorem ipsum blah blah blah","image":"media/car.jpg"},{"title":"NXP Thing","content":"Lorem ipsum blah blah blah","image":"media/car.jpg"}]
+var contentJson = [{"title":"NXP Thing","description":"Lorem ipsum blah blah blah","image":"media/car.jpg","tags":["electrical","funny","rf"]},{"title":"NXP Thing","description":"Yup yup blah blah blah","image":"media/ar.jpg","tags":["firmware","rf"]},{"title":"Submarine","image":"media/sub.jpg","description":"I'm on a boat!","tags":["funny","under-water"]}]
--- static/js/base.js
+++ static/js/base.js
@@ -1,28 +1,48 @@
-card1 = {
-    title : "NXP Thing",
-    content : "Lorem ipsum blah blah blah",
-    image : "media/car.jpg"
-};
+function exportContent() {
+  console.log("Exporting");
+  json = JSON.stringify(content.entries);
+  json = "var contentJson = " + json;
+  console.log(json);
+  var blob = new Blob([json], {type: "application/js"});
+  var url  = URL.createObjectURL(blob);
+  var a = document.createElement('a');
+  a.download = "content.js";
+  a.href = url;
+  a.click();
+}
 
-card2 = {
-    title : "NXP Thing",
-    content : "Lorem ipsum blah blah blah",
-    image : "media/car.jpg"
-};
+Vue.component('entry-card', {
+  props: ['entry'],
+  template:`
+    <div class="col-md-6">
+      <div class="card">
+        <img class="card-img-top" style="width:100%" :src="entry.image">
+        <div class="card-block">
+          <h4 class="card-title">{{ entry.title }}</h4>
+          <p class="card-text">{{ entry.description }}</p>
+          <a href="#" class="btn btn-outline-primary">more info</a>
+          <div><button type="button" class="btn btn-secondary btn-sm" v-for="tag in entry.tags">{{ tag }}</button></div>
+        </div>
+      </div>
+    </div>
+    `
+});
 
-cards = []
-cards.push(card1)
-cards.push(card2)
+var content = new Vue({
+  el: '#content',
+  data: {
+    entries: contentJson
+  }
+});
 
-json = JSON.stringify(cards)
-json = "var contentJson = " + json
-console.log(json);
+$('#editForm').submit(function(e) {
+    e.preventDefault();
+    entry = {};
+    entry.title = $('#titleInput').val();
+    entry.image = $('#imageInput').val();
+    entry.description = $('#descriptionInput').val();
+    entry.tags = $('#tagsInput').val().split(',');
 
-var blob = new Blob([json], {type: "application/js"});
-var url  = URL.createObjectURL(blob);
-var b = $("#saveButton");
-b.attr("download", "content.js");
-b.attr("href", url);
+    content.entries.push(entry);
 
-console.log("Read in the data")
-console.log(contentJson)
+});
