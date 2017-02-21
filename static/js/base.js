71bdf41dccfeb7d4769a243352362da9768031d2
aninternetof <bradyhurlburt@gmail.com>
2017-02-21 17:36:22 -0500

    Adds editing ability

--- static/js/base.js
+++ static/js/base.js
@@ -1,16 +1,3 @@
-function exportContent() {
-  console.log("Exporting");
-  json = JSON.stringify(content.entries);
-  json = "var contentJson = " + json;
-  console.log(json);
-  var blob = new Blob([json], {type: "application/js"});
-  var url  = URL.createObjectURL(blob);
-  var a = document.createElement('a');
-  a.download = "content.js";
-  a.href = url;
-  a.click();
-}
-
 Vue.component('entry-card', {
   props: ['entry'],
   template:`
@@ -19,8 +6,9 @@ Vue.component('entry-card', {
         <img class="card-img-top" style="width:100%" :src="entry.image">
         <div class="card-block">
           <h4 class="card-title">{{ entry.title }}</h4>
+          <a :id=entry.title onclick="editEntry(event)" class="card-text" href="#">edit</a>
           <p class="card-text">{{ entry.description }}</p>
-          <a href="#" class="btn btn-outline-primary">more info</a>
+          <a :href="entry.link" class="btn btn-outline-primary">more info</a>
           <div><button type="button" class="btn btn-secondary btn-sm" v-for="tag in entry.tags">{{ tag }}</button></div>
         </div>
       </div>
@@ -32,8 +20,53 @@ var content = new Vue({
   el: '#content',
   data: {
     entries: contentJson
-  }
+  },
 });
+
+function removeEntry(title) {
+  var toRemove = [];
+  for (var i=0; i<content.entries.length; i++) {
+    if (content.entries[i].title == title) {
+      toRemove.push(i);
+    }
+  }
+  console.log(toRemove);
+  for (var j=0; j<toRemove.length; j++) {
+    content.entries.splice(toRemove[j]-j,1);
+  }
+}
+
+function getEntry(title) {
+  for (var i=0; i<content.entries.length; i++) {
+    if (content.entries[i].title == title) {
+      return content.entries[i];
+    }
+  }
+}
+
+function editEntry(event) {
+  event.preventDefault();
+  console.log(event.target.id);
+  entry = getEntry(event.target.id);
+  console.log(entry);
+  $('#titleInput').val(entry.title);
+  $('#imageInput').val(entry.image);
+  $('#descriptionInput').val(entry.description);
+  $('#linkInput').val(entry.link);
+  $('#tagsInput').val(entry.tags.join());
+  $('#editModal').modal('show');
+}
+
+function exportContent() {
+  json = JSON.stringify(content.entries);
+  json = "var contentJson = " + json;
+    var blob = new Blob([json], {type: "application/js"});
+  var url  = URL.createObjectURL(blob);
+  var a = document.createElement('a');
+  a.download = "content.js";
+  a.href = url;
+  a.click();
+}
 
 $('#editForm').submit(function(e) {
     e.preventDefault();
@@ -41,8 +74,10 @@ $('#editForm').submit(function(e) {
     entry.title = $('#titleInput').val();
     entry.image = $('#imageInput').val();
     entry.description = $('#descriptionInput').val();
+    entry.link = $('#linkInput').val();
     entry.tags = $('#tagsInput').val().split(',');
-
+    /* get rid of any others with the same name */
+    removeEntry(entry.title)
     content.entries.push(entry);
-
+    $('#editModal').modal('hide');
 });
